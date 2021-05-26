28a39bd92627c87027d35c29416ffe4073d9e7bc
wjh <1787102205@qq.com>
2021-05-26 13:08:01 +0800

    fisrt commit in hot-fix

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -3,5 +3,6 @@ public class GitTest {
         System.out.println("好耶！");
         System.out.println("好耶！好耶！");
         System.out.println("好耶！在hot-fix分支里的第一次提交");
+        System.out.println("push test");
     }
 }
